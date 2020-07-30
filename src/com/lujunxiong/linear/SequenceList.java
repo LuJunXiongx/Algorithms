82c4f88508fb04fd13fb82f2dd86f6efc14dad13
LuJunXiongX <1937480486@qq.com>
2020-07-30 09:59:53 +0800

    顺序表扩容缩容操作

--- src/com/lujunxiong/linear/SequenceList.java
+++ src/com/lujunxiong/linear/SequenceList.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
  * @Date 2020/7/29
  * @Description: 顺序表api
  **/
-public class SequenceList<T> implements Iterable<T>{
+public class SequenceList<T> implements Iterable<T> {
 
     /**
      * 存储元素的数组
@@ -63,6 +63,9 @@ public class SequenceList<T> implements Iterable<T>{
      * 向线性表中添加元素
      */
     public void insert(T t) {
+        if (N==eles.length){
+            resize(2*eles.length);
+        }
         eles[N++] = t;
     }
 
@@ -70,6 +73,9 @@ public class SequenceList<T> implements Iterable<T>{
      * 在i元素插入元素t
      */
     public void insert(int i, T t) {
+        if (N==eles.length){
+            resize(2*eles.length);
+        }
         //先把i索引处的元素及其后面的元素依次向后移动一位
         for (int index = N; index > i; index--) {
             eles[index] = eles[index - 1];
@@ -80,47 +86,69 @@ public class SequenceList<T> implements Iterable<T>{
         //元素个数+1
         N++;
     }
+
     /**
      * 删除指定位置i处的元素，并返回该元素
      */
-    public T remove(int i){
+    public T remove(int i) {
         //记录索引i的值
         T current = eles[i];
         //索引i后面的元素依次向前移动即可
-        for (int index=i;index<N-1;index++){
-            eles[index]=eles[index+1];
+        for (int index = i; index < N - 1; index++) {
+            eles[index] = eles[index + 1];
 
         }
         //元素个数减一
         N--;
+
+        if (N<eles.length/4){
+            resize(eles.length/2);
+        }
         return current;
     }
 
     /**
      * 查找指定元素第一次出现的位置
      */
-    public int indexOf(T t){
-        for (int i=0;i<N;i++){
-            if (eles[i].equals(t)){
+    public int indexOf(T t) {
+        for (int i = 0; i < N; i++) {
+            if (eles[i].equals(t)) {
                 return i;
             }
         }
         return -1;
     }
 
+    /**
+     * 根据参数newSize，重置eles的大小
+     * @param newSize
+     */
+    public void resize(int newSize) {
+        //定义一个临时数组，指向原数组
+        T[] temp=eles;
+        //创建新数组
+        eles=(T[])new Object[newSize];
+        //把原数组的数据拷贝到新数组即可
+        for(int i=0;i<N;i++){
+            eles[i]=temp[i];
+        }
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new SIterator();
     }
-    private class SIterator implements Iterator{
+
+    private class SIterator implements Iterator {
         private int cusor;
-        public SIterator(){
-            this.cusor=0;
+
+        public SIterator() {
+            this.cusor = 0;
         }
 
         @Override
         public boolean hasNext() {
-            return cusor<N;
+            return cusor < N;
         }
 
         @Override
---
+++ src/com/lujunxiong/test/SequenceListTest2.java
@@ -0,0 +1,24 @@
+package com.lujunxiong.test;
+
+import com.lujunxiong.linear.SequenceList;
+
+/**
+ * @ClassName SequenceListTest2
+ * @Author 陆军雄
+ * @Date 2020/7/30
+ * @Description: 顺序表扩容缩容测试
+ **/
+public class SequenceListTest2 {
+    public static void main(String[] args) {
+        SequenceList<String> sl = new SequenceList<>(3);
+        sl.insert("张三");
+        sl.insert("李四");
+        sl.insert("王五");
+        sl.insert("赵六");
+
+
+
+
+
+    }
+}
